eb7705fb50ab66e8acb060ce0f81aa8760edcb80
Mohammed M Khalid <mushfick@users.noreply.github.com>
2017-11-11 10:04:51 -0800

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,38 @@
+defmodule Issues.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :issues,
+      name: "Issues",
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      escript: escript_config(),
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:httpoison, "~> 0.13.0"}, 
+      {:ex_doc, "~> 0.18.1"},
+      {:earmark, "~> 1.2"},
+      {:poison, "~> 3.1.0"}
+    ]
+  end
+
+  defp escript_config do
+    [ main_module: Issues.CLI ]
+  end
+end
